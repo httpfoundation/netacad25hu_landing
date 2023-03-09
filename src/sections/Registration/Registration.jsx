04616ea951000f097e24711750ce287e178e8d35
Zoltan <zoltan.sisak@gmail.com>
2023-03-09 16:55:36 +0100

    registration options

--- src/sections/Registration/Registration.jsx
+++ src/sections/Registration/Registration.jsx
@@ -24,7 +24,14 @@ const Registration = (props) => {
 	const [registrationFormatVipText] = useStaticElement("registrationFormatVip")
 	const [registrationSuccessText] = useStaticElement("registrationSuccess") 
 	const [registrationFormatCheckboxText] = useStaticElement("registrationFormatCheckbox", false) 	
-
+	const [registrationOnline] = useStaticElement("registrationOnline", false) 	
+	const [registrationOnsite] = useStaticElement("registrationOnsite", false)
+	const [registrationInfoOnlineOnly] = useStaticElement("registrationInfoOnlineOnly")
+	const [registrationInfoOnsiteOnly] = useStaticElement("registrationInfoOnsiteOnly")
+	const [registrationInfoHybrid] = useStaticElement("registrationInfoHybrid")
+	const [registrationInfoClosed] = useStaticElement("registrationInfoClosed")
+	console.log({registrationOnline}, {registrationOnsite}, {registrationInfoOnlineOnly: registrationInfoOnlineOnly}, {registrationInfoOnsiteOnly: registrationInfoOnsiteOnly}, {registrationInfoHibryd: registrationInfoHybrid})
+	const registrationText2 = registrationOnline && registrationOnsite ? registrationInfoHybrid : registrationOnline ? registrationInfoOnlineOnly : registrationInfoClosed
 
 	const [name, setName] = useState('')
 	const [email, setEmail] = useState('')
@@ -105,93 +112,100 @@ const Registration = (props) => {
 	return <Section id="regisztracio" container placeholder>
 		<Title>Biztosítsd már most a <span className="highlight text-uppercase">helyed</span>!</Title>
 		<Text subtitle>
-			<StructuredText data={registrationText} />	
+			<StructuredText data={registrationText2} />
 		</Text>
-		<form className="reg-form" onSubmit={onSubmit}>
-			<Title subtitle>Add meg az adataidat!</Title>
+		{(registrationOnsite || registrationOnline) && (<>
 
-			<Alert variant="success" show={vipCode && error !== "vip"}>
-				VIP regisztrációs kód aktiválva
-			</Alert>
+			<form className="reg-form" onSubmit={onSubmit}>
+				<Title subtitle>Add meg az adataidat!</Title>
 
-			
-			<label className="form-label" htmlFor="name-field">Név*</label>
-			<input id="name-field" className="form-control" value={name} onChange={e => setName(e.target.value)} autoComplete="name" required/>
+				<Alert variant="success" show={vipCode && error !== "vip"}>
+					VIP regisztrációs kód aktiválva
+				</Alert>
 
-			<label className="form-label" htmlFor="email-field">E-mail cím*</label>
-			<input id="email-field" className={`form-control ${error === "email" ? 'is-invalid' : ''}`} value={email} onChange={e => setEmail(e.target.value)} autoComplete="email" required/>
-			<Alert variant="danger" show={error === "email"}>
-				Ezzel az e-mail címmel már történt regisztráció.
-			</Alert>
+				
+				<label className="form-label" htmlFor="name-field">Név*</label>
+				<input id="name-field" className="form-control" value={name} onChange={e => setName(e.target.value)} autoComplete="name" required/>
 
-			<label className="form-label" htmlFor="phone-field">Telefonszám*</label>
-			<input id="phone-field" className="form-control" value={phone} onChange={e => setPhone(e.target.value)} autoComplete="tel" required/>
+				<label className="form-label" htmlFor="email-field">E-mail cím*</label>
+				<input id="email-field" className={`form-control ${error === "email" ? 'is-invalid' : ''}`} value={email} onChange={e => setEmail(e.target.value)} autoComplete="email" required/>
+				<Alert variant="danger" show={error === "email"}>
+					Ezzel az e-mail címmel már történt regisztráció.
+				</Alert>
 
-			<label className="form-label" htmlFor="workplace-field">Munkahely*</label>
-			<input id="workplace-field" className="form-control" value={workplace} onChange={e => setWorkplace(e.target.value)} autoComplete="organization" required/>
+				<label className="form-label" htmlFor="phone-field">Telefonszám*</label>
+				<input id="phone-field" className="form-control" value={phone} onChange={e => setPhone(e.target.value)} autoComplete="tel" required/>
 
-			<label className="form-label" htmlFor="city-field">Település*</label>
-			<input id="city-field" className="form-control" value={city} onChange={e => setCity(e.target.value)} autoComplete="address-level2" required/>
+				<label className="form-label" htmlFor="workplace-field">Munkahely*</label>
+				<input id="workplace-field" className="form-control" value={workplace} onChange={e => setWorkplace(e.target.value)} autoComplete="organization" required/>
 
-			<label className="form-label">Jelentkezés személyes részvételre</label>
-			<div className="" style={{padding: '0.8rem', border: '1px solid #ced4da', borderRadius: '0.25rem'}}>
-				<StructuredText data={vipCode ? registrationFormatVipText : registrationFormatText} />
-				<div className="form-check">
-					<input className="form-check-input" type="checkbox" name="online" id="onsite-field" checked={onsite} onChange={e => setOnsite(e.target.checked)}/>
-					<label className="form-check-label" htmlFor="onsite-field">
-						{registrationFormatCheckboxText}
+				<label className="form-label" htmlFor="city-field">Település*</label>
+				<input id="city-field" className="form-control" value={city} onChange={e => setCity(e.target.value)} autoComplete="address-level2" required/>
+
+				{ registrationOnsite && (<>
+					<label className="form-label">Jelentkezés személyes részvételre</label>
+					<div className="" style={{padding: '0.8rem', border: '1px solid #ced4da', borderRadius: '0.25rem'}}>
+						<StructuredText data={vipCode ? registrationFormatVipText : registrationFormatText} />
+						<div className="form-check">
+							<input className="form-check-input" type="checkbox" name="online" id="onsite-field" checked={onsite} onChange={e => setOnsite(e.target.checked)}/>
+							<label className="form-check-label" htmlFor="onsite-field">
+								{registrationFormatCheckboxText}
+							</label>
+						</div>
+						{ onsite && 
+							<>
+								<label className="form-label  mt-4">Melyik délutáni szekción szeretnél részt venni?*</label>
+								<select className="form-select" required={onsite} value={stage} onChange={e => setStage(e.target.value)}>
+									<option value={""} hidden></option>
+									{ allStages?.slice(1).slice(0,-1).map((stage, index) => <option key={index} value={stage.id}>{stage.name}</option>) }
+								</select>
+							</>
+						}
+					</div>
+				</>)
+				
+				}
+				<div className="form-check mb-4 mt-4">
+					<input className="form-check-input" type="checkbox" name="newsletter" id="newsletter-field" checked={newsletter} onChange={e => setNewsletter(e.target.checked)}/>
+					<label className="form-check-label" htmlFor="newsletter-field">
+						Szeretnék emailben értesülni az InfoTanár Mentor programmal kapcsolatos információkról
 					</label>
 				</div>
-				{ onsite && 
-					<>
-						<label className="form-label  mt-4">Melyik délutáni szekción szeretnél részt venni?*</label>
-						<select className="form-select" required={onsite} value={stage} onChange={e => setStage(e.target.value)}>
-							<option value={""} hidden></option>
-							{ allStages?.slice(1).slice(0,-1).map((stage, index) => <option key={index} value={stage.id}>{stage.name}</option>) }
-						</select>
-					</>
-				}
-			</div>
-			<div className="form-check mb-4 mt-4">
-				<input className="form-check-input" type="checkbox" name="newsletter" id="newsletter-field" checked={newsletter} onChange={e => setNewsletter(e.target.checked)}/>
-				<label className="form-check-label" htmlFor="newsletter-field">
-					Szeretnék emailben értesülni az InfoTanár Mentor programmal kapcsolatos információkról
-				</label>
-			</div>
-			<div className="form-check mb-4 mt-4">
-				<input className="form-check-input" type="checkbox" id="toc-field" required />
-				<label className="form-check-label" htmlFor="toc-field">
-					Elolvastam és elfogadom az <a href="https://www.datocms-assets.com/94181/1676993398-adatkezelesi_tajekoztato_iok2023.pdf" target="_blank" className="link" rel="noreferrer">Adatkezelési Tájékoztató</a>ban foglaltakat.*
-				</label>
-			</div>			
-			<div className="my-4"/>
+				<div className="form-check mb-4 mt-4">
+					<input className="form-check-input" type="checkbox" id="toc-field" required />
+					<label className="form-check-label" htmlFor="toc-field">
+						Elolvastam és elfogadom az <a href="https://www.datocms-assets.com/94181/1676993398-adatkezelesi_tajekoztato_iok2023.pdf" target="_blank" className="link" rel="noreferrer">Adatkezelési Tájékoztató</a>ban foglaltakat.*
+					</label>
+				</div>			
+				<div className="my-4"/>
 
-			<Alert variant="danger" show={error === "other"}>
-				Ismeretlen hiba történt a jelentkezés során. Kérlek, próbáld újra később.
-			</Alert>
-			
-			<Alert variant="danger" show={error === "vip"}>
-				Érvénytelen VIP regisztrációs kód.
-			</Alert>
+				<Alert variant="danger" show={error === "other"}>
+					Ismeretlen hiba történt a jelentkezés során. Kérlek, próbáld újra később.
+				</Alert>
+				
+				<Alert variant="danger" show={error === "vip"}>
+					Érvénytelen VIP regisztrációs kód.
+				</Alert>
 
-			<Alert variant="danger" show={error === "email"}>
-				A megadott e-mail címmel már történt regisztráció.
-			</Alert>
+				<Alert variant="danger" show={error === "email"}>
+					A megadott e-mail címmel már történt regisztráció.
+				</Alert>
 
-			<Button submit>
-				{ loading &&
-				<div style={{ position: 'absolute', top: '5px', left: '50%', transform: 'translateX(-50%)' }}>
-					<Spinner
-						as="span"
-						animation="border"
-						role="status"
-						aria-hidden="true"
-					/>
-				</div>
-				}
-				<span style={{opacity: loading ? 0 : 1}}>Regisztráció</span>
-			</Button>
-		</form>
+				<Button submit>
+					{ loading &&
+					<div style={{ position: 'absolute', top: '5px', left: '50%', transform: 'translateX(-50%)' }}>
+						<Spinner
+							as="span"
+							animation="border"
+							role="status"
+							aria-hidden="true"
+						/>
+					</div>
+					}
+					<span style={{opacity: loading ? 0 : 1}}>Regisztráció</span>
+				</Button>
+			</form>
+		</>)}
 
 
 		<Modal show={success} onHide={() => {setSuccess(false)}} centered>
